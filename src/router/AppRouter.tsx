5869b869780e4e61c40c5d6c073055dc8bb767dc
seedk76 <39347934+seedk76@users.noreply.github.com>
2025-02-06 17:00:21 +0200

    Update AppRouter.tsx

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App';
 import LatestDeployments from '../pages/latest-deployments';
+import Solutions from '../pages/solutions';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
-  { 
-  path: '/solutions',
-  element: <Solutions />,
- },
   {
     path: '/latest-deployments',
     element: <LatestDeployments />,
+  },
+  {
+    path: '/solutions',
+    element: <Solutions />,
   }
 ]);
 
