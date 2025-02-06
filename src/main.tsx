784451fb7cff0b062dcddab1658cd13a8a735ea9
seedk76 <39347934+seedk76@users.noreply.github.com>
2025-02-06 16:51:53 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppRouter from './router/AppRouter';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>
+);
